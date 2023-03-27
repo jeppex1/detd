97c140a18bd6e46465cff4237d2b383832f06edd
jeppex1 <41836686+jeppex1@users.noreply.github.com>
2023-03-27 13:18:06 +0200

    hardware latency for i225 and rename for i210

--- detd/devices/intel_i210.py
+++ detd/devices/intel_i210.py
@@ -30,9 +30,9 @@ class IntelI210(Device):
     
     CAPABILITIES  = [Capability.LTC]
 
-    #MAC+PHY delay(Qav Latencies) from datasheet assuming 100 mbit link
-    HARDWARE_DELAY_MIN = 2168
-    HARDWARE_DELAY_MAX = 2384
+    #MAC+PHY latency assuming 100 mbit link
+    HARDWARE_LATENCY_MIN = 2168
+    HARDWARE_LATENCY_MAX = 2384
 
     PCI_IDS_VALID = ['8086:1533', '8086:1536', '8086:1537', '8086:1538', '8086:157B',
                      '8086:157C', '8086:15F6']
@@ -46,7 +46,7 @@ class IntelI210(Device):
 
         logger.info(f"Initializing {__class__.__name__}")
 
-        super().__init__(IntelI210.NUM_TX_QUEUES, IntelI210.NUM_RX_QUEUES, IntelI210.HARDWARE_DELAY_MIN, IntelI210.HARDWARE_DELAY_MAX)
+        super().__init__(IntelI210.NUM_TX_QUEUES, IntelI210.NUM_RX_QUEUES, IntelI210.HARDWARE_LATENCY_MIN, IntelI210.HARDWARE_LATENCY_MAX)
 
         self.capabilities = [Capability.LTC]
 
